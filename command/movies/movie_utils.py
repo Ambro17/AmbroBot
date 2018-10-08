bc0e94f83657e286a6cc74cbbfa8f92b55606787
Nahuel Ambrosini <nambrosini@onapsis.com>
2018-10-07 23:20:47 -0300

    Refactor movie_utils to retrieve only basic info. And retrieve imdb, yt and .torrent links if user explicitly asks it. (Faster)

--- command/movies/movie_utils.py
+++ command/movies/movie_utils.py
@@ -8,91 +8,59 @@ TMDB_KEY = '7f76943e1557e33276e0f595c2128f68'
 logger = logging.getLogger(__name__)
 
 
-def search_movie(pelicula):
-    params = {'api_key': TMDB_KEY, 'query': pelicula, 'language': 'es-AR'}
+def get_movie(pelicula_query):
+    params = {'api_key': TMDB_KEY, 'query': pelicula_query, 'language': 'es-AR'}
     r = requests.get('https://api.themoviedb.org/3/search/movie', params=params)
     if r.status_code == 200:
-        movies = r.json()['results']
-        if movies:
-            movie_info = get_movie_info(movies[0])
-            message = prettify_movie_info(*movie_info)
-        else:
-            message = 'No conozco ninguna peli bajo ese nombre..'
-    else:
-        message = 'Imdb se fue de vacaciones'
+        try:
+            movies = r.json()['results'][0]
+            return movies
+        except (IndexError, KeyError):
+            return None
+
+
+def prettify_movie(movie_dict):
+    movie_info = get_basic_info(movie_dict)
+    message = prettify_basic_movie_info(*movie_info)
 
     return message
 
 
-def get_movie_info(movie):
+def get_basic_info(movie):
     title = movie['title']
     rating = movie['vote_average']
     overview = movie['overview']
     year = movie['release_date'].split('-')[0]  # "2016-07-27" -> 2016
-    # Get yt_trailer, imdb_link and magnet.
-    params = {'api_key': TMDB_KEY, 'append_to_response': 'videos'}
-    r = requests.get(f"https://api.themoviedb.org/3/movie/{movie['id']}", params=params)
-    data = r.json()
-    imdb_id = data['imdb_id']
-    imdb_link = f"http://www.imdb.com/title/{imdb_id}"
-    yt_trailer = get_yt_trailer(data)
-    magnet = get_magnet_link(title, imdb_id)
-    return title, rating, overview, year, imdb_link, yt_trailer, magnet
+    return title, rating, overview, year
 
 
-def get_yt_trailer(data):
-    key = data['videos']['results'][0]['key']
-    return f'https://www.youtube.com/watch?v={key}'
-
-
-def prettify_movie_info(title, rating, overview, year, imdb, yt, magnet):
+def prettify_basic_movie_info(title, rating, overview, year):
     stars = int(rating // 2)
-    imdb_line = f'[🎭 IMDB]({imdb})'
-    yt_line = f'[▶ Trailer]({yt})'
-    if magnet:
-        magnet = f"🏴‍☠[MAGNET]({magnet_to_torrent(magnet)})"
     return (
         f"{title} ({year})\n"
-        f"{'⭐'*stars}\n\n"
+        f"{'⭐'*stars} ~ {rating}\n\n"
         f"{overview}\n\n"
-        f"{imdb_line} | {yt_line}" + f" | {magnet}"
-        if magnet
-        else ''
     )
 
 
-def get_trackers():
-    "URL encode trackers with &tr=prefix"
-    trackers = [
-        'udp://tracker.opentrackr.org:1337/announce',
-        'udp://torrent.gresille.org:80/announce',
-        'udp://tracker.openbittorrent.com:80',
-        'udp://tracker.coppersurfer.tk:6969',
-        'udp://tracker.leechers-paradise.org:6969',
-        'udp://p4p.arenabg.ch:1337',
-        'udp://tracker.internetwarriors.net:1337',
-    ]
-
-    return ''.join(f'&tr={tracker}' for tracker in trackers)
+def get_yt_trailer(videos):
+    key = videos['results'][0]['key']
+    return f'https://www.youtube.com/watch?v={key}'
 
 
-def get_magnet_link(title, imdb_id):
-    MAGNET = "magnet:?xt=urn:btih:{torrent_hash}&dn={movie_name}{trackers}"
-    jash = request_hash_from_yts(imdb_id)
-    if jash:
-        return MAGNET.format(
-            torrent_hash=jash, movie_name=encode_for_url(title), trackers=get_trackers()
-        )
-    return None
-
-
-def request_hash_from_yts(imdb_id):
-    BEST_QUALITY = -1
+def get_torrent_info(imdb_id):
     yts_api = 'https://yts.am/api/v2/list_movies.json'
     r = requests.get(yts_api, params={"query_term": imdb_id})
     if r.status_code == 200:
-        torrent = r.json()
+        torrent = r.json() # Dar url en lugar de hash.
         try:
-            return torrent["data"]["movies"][0]["torrents"][BEST_QUALITY]["hash"]
-        except Exception as e:
-            logger.exception("Something bad happened")
+            movie = torrent["data"]["movies"][0]['torrents'][0]
+            url = movie['url']
+            seeds = movie['seeds']
+            size = movie['size']
+            quality = movie['quality']
+
+            return url, seeds, size, quality
+
+        except (IndexError, KeyError) as e:
+            logger.exception("There was a problem with yts api response")
