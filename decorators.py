92e4fe2b5109ce5ac3d95e288bbbbf8c4702261f
Nahuel Ambrosini <nambrosini@onapsis.com>
2018-10-07 21:06:17 -0300

    Add admin_only decorator

--- decorators.py
+++ decorators.py
@@ -2,6 +2,8 @@ import logging
 import time
 
 from functools import wraps
+
+import os
 from telegram import ChatAction
 
 logger = logging.getLogger(__name__)
@@ -10,8 +12,7 @@ def send_action(action):
     """Sends `action` while processing func command."""
     def decorator(func):
         @wraps(func)
-        def command_func(*args, **kwargs):
-            bot, update = args
+        def command_func(bot, update, **kwargs):
             bot.send_chat_action(chat_id=update.message.chat_id, action=action)
             func(bot, update, **kwargs)
         return command_func
@@ -32,3 +33,18 @@ def log_time(func):
         logger.info(f"[{func.__name__}] <{end-start}> {kwargs if kwargs else ''}")
         return result
     return wrapped_func
+
+
+
+def admin_only(func):
+    @wraps(func)
+    def restricted_func(bot, update, **kwargs):
+        user = update.effective_user.username
+        if user == os.environ['admin']:
+            func(bot, update, **kwargs)
+        else:
+            logger.info("User %s not authorized to perform action.", user)
+    return restricted_func
+
+
+
