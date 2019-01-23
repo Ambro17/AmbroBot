a0b854d8f57a28a36d36fd0296b6ee14af3c64a1
Ambro17 <ambro17.1@gmail.com>
2019-01-23 20:20:36 -0300

    Filter clickable bancos to those that are more popular to avoid crumbling too much info

--- commands/dolar/callback.py
+++ commands/dolar/callback.py
@@ -3,9 +3,6 @@ from commands.dolar.utils import pretty_print_dolar
 
 def dolarhoy_callback(banco_data, banco):
     """Shows only the info of desired banco from banco_data"""
-    # Maybe we need some asociation between banco callback_data and the name of the key on the dict.
-    # -> For now, callback data will match banco_data key, but this may be error prone (can we avoid that, tho?)
-    # Filtrar en el dict la entrada que quiero o crear otra
     requested_banco = {k: v for k, v in banco_data.items() if k == banco}
     if requested_banco:
         return pretty_print_dolar(requested_banco)
--- commands/dolar/keyboards.py
+++ commands/dolar/keyboards.py
@@ -1,11 +1,21 @@
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton as Button
 
+POPULAR_TRADE_HOUSES = (
+    'Galicia',
+    'Frances',
+    'Nacion',
+    'Santander',
+    'Mayorista',
+    'Dolar bolsa',
+)
+
 
 def banco_keyboard(cotizaciones):
     COLUMNS = 3
     buttons = [
         Button(f'{banco}', callback_data=banco)
         for banco in cotizaciones
+        if banco in POPULAR_TRADE_HOUSES
     ]
     columned_keyboard = [
         buttons[i: i + COLUMNS] for i in range(0, len(buttons), COLUMNS)
